91f482eacbe9df9f964671525c163ef30f03bab0
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2021-06-09 09:52:08 +0200

    regrid to worldmap with time dimension but fails so far

--- test_rf.py
+++ test_rf.py
@@ -10,15 +10,16 @@ import cartopy.crs as ccrs
 from sklearn.ensemble import RandomForestRegressor
 
 largefilepath = '/net/so4/landclim/bverena/large_files/'
-case = 'var'
+case = 'yearly'
 
 def to_latlon(data):
+    t = data.time.shape[0]
     lsmfile = '/net/exo/landclim/data/dataset/ERA5_deterministic/recent/0.25deg_lat-lon_time-invariant/processed/regrid/era5_deterministic_recent.lsm.025deg.time-invariant.nc'
     lsm = xr.open_mfdataset(lsmfile, combine='by_coords')
     shape = lsm['lsm'].squeeze().shape
     landlat, landlon = np.where((lsm['lsm'].squeeze() > 0.8).load()) # land is 1, ocean is 0
-    tmp = xr.DataArray(np.full((shape[0],shape[1]),np.nan), coords=[lsm.coords['lat'], lsm.coords['lon']], dims=['lat','lon'])
-    tmp.values[landlat,landlon] = data
+    tmp = xr.DataArray(np.full((t,shape[0],shape[1]),np.nan), coords=[data.coords['time'],lsm.coords['lat'], lsm.coords['lon']], dims=['time','lat','lon'])
+    tmp.values[:,landlat,landlon] = data.T
     return tmp
 
 # load data
@@ -47,12 +48,12 @@ kwargs = {'n_estimators': n_trees,
           'n_jobs': None, # set to number of trees
           'verbose': 0}
 rf = RandomForestRegressor(**kwargs)
-rf.fit(X_train.T, y_train)
+rf.fit(X_train, y_train)
 
-res = np.zeros((n_trees, X_test.T.shape[0]))
+res = np.zeros((n_trees, X_test.datapoints.shape[0]))
 for t, tree in enumerate(rf.estimators_):
     print(t)
-    res[t,:] = tree.predict(X_test.T)
+    res[t,:] = tree.predict(X_test)
 mean = np.mean(res, axis=0)
 upper, lower = np.percentile(res, [95 ,5], axis=0)
 
@@ -62,12 +63,15 @@ y_predict = xr.full_like(y_test, np.nan)
 y_unc = xr.full_like(y_test, np.nan)
 y_predict[:] = mean
 y_unc[:] = (upper - lower)
+import IPython; IPython.embed()
+datamap = to_latlon(xr.concat([y_test, y_train_empty], dim='datapoints').set_index(datapoints=("time", "landpoints")).unstack("datapoints"))
 datamap = to_latlon(xr.concat([y_test, y_train_empty], dim='landpoints'))
 predmap = to_latlon(xr.concat([y_predict, y_train_empty], dim='landpoints'))
 uncmap = to_latlon(xr.concat([y_unc, y_train_empty], dim='landpoints'))
 datamap = datamap * datastd + datamean
 predmap = predmap * datastd + datamean
 uncmap = uncmap * datastd 
+import IPython; IPython.embed()
 
 # plot prediction and uncertainty
 proj = ccrs.PlateCarree()
