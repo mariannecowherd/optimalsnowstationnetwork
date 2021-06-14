3a8101fa1b9686472f19fe3758b6ccdbd9110f4b
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2021-06-14 17:19:23 +0200

    clean up pd csv all info in one script

--- preproc_ismn.py
+++ preproc_ismn.py
@@ -4,10 +4,10 @@ extract lat lon and time information from ISMN network
 
 import glob
 from datetime import datetime
+import xarray as xr
 import numpy as np
 import pandas as pd
 
-# TODO add time of each station
 # TODO add vegetation of each station
 # TODO use Martins apporach (see Email)
 
@@ -22,6 +22,7 @@ df = pd.DataFrame(index=index, columns=columns)
 
 i = 0
 for folder in station_folders:
+    print(folder)
     try:
         onefile = glob.glob(f'{folder}*.stm', recursive=True)[0]
     except IndexError: # list index out of range -> no .stm files in this directory
@@ -40,4 +41,42 @@ for folder in station_folders:
     df.end[i] = station_end
     i += 1
 
-df.to_csv(f'{largefilepath}station_info.csv')
+# interpolate station locations on era5 grid
+filepath = '/net/exo/landclim/data/dataset/ERA5_deterministic/recent/0.25deg_lat-lon_time-invariant/processed/regrid/'
+filename = f'{filepath}era5_deterministic_recent.lsm.025deg.time-invariant.nc'
+data = xr.open_dataset(filename)
+def find_closest(list_of_values, number):
+    return min(list_of_values, key=lambda x:abs(x-number))
+station_grid_lat = []
+station_grid_lon = []
+for lat, lon in zip(df.lat,df.lon):
+    print(lat, lon)
+    station_grid_lat.append(find_closest(data.lat, lat))
+    station_grid_lon.append(find_closest(data.lon, lon))
+df['lat_grid'] = station_grid_lat
+df['lon_grid'] = station_grid_lon
+
+# koeppen climate class per location
+filename = f'{largefilepath}Beck_KG_V1_present_0p0083.tif'
+koeppen = xr.open_rasterio(filename)
+koeppen = koeppen.rename({'x':'lon','y':'lat'}).squeeze()
+
+koeppen_class = []
+for lat, lon in zip(station_grid_lat,station_grid_lon):
+    print(lat, lon)
+    koeppen_class.append(koeppen.sel(lon=lon, lat=lat).values.item())
+df['koeppen_class'] = koeppen_class
+station_coords.to_csv(f'{largefilepath}station_info_grid.csv')
+
+quit()
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+z,x,y = pltarr.values.nonzero()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x, y, -z, zdir='z', c= 'red')
+plt.show()
+
+# plot 3D lat lon time plot!
+for lat, lon, start, end in zip(df.lat, df.lon, df.start, df.end):
+    pltarr.loc[slice(start,end),lat,lon] = 1
