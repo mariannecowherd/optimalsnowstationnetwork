e8eb47f2a34ab6f362b5ade4e012f12231583c11
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2021-06-18 12:10:37 +0200

    other approach for file filtering, simplified koeppen classes, vegetation classes, remove stations in ocean

--- preproc_ismn.py
+++ preproc_ismn.py
@@ -8,10 +8,16 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 
-# TODO add vegetation of each station
-# TODO use Martins apporach (see Email)
-# TODO remove stations that are placed in ocean koeppen class
-# TODO add reduced koeppen class
+# add vegetation of each station; done, funny vegetation descriptions
+# use Martins apporach (see Email) # not done because less stations (soil moisture layer depths are not standardised)
+# see for example "find . -type f" in ismnpath
+# remove stations that are placed in ocean koeppen class; done, 27 stations removed, all coastal (no obvious wrong coords)
+# add reduced koeppen class; done
+# TODO see whether there are measurement gaps
+# started with: 
+# test = pd.read_csv(onefile, skiprows=1, header=None, delimiter='  ', engine='python')
+# test.iloc[:,0] = [datetime.strptime(date, '%Y/%m/%d %H:%M').date() for date in test.iloc[:,0]]
+# test.columns = ['date','value']
 
 ismnpath = '/net/exo/landclim/data/variable/soil-moisture/ISMN/20210211/point-scale_none_0.5h/original/'
 largefilepath = '/net/so4/landclim/bverena/large_files/'
@@ -19,16 +25,17 @@ station_folders = glob.glob(f'{ismnpath}**/', recursive=True)
 
 # create empty pandas dataframe for data
 print('find all station information')
-index = range(2827)
-columns = ['lat','lon','start','end']
+index = range(2816)
+columns = ['lat','lon','start','end', 'land_cover_class']
 df = pd.DataFrame(index=index, columns=columns)
 
 i = 0
 for folder in station_folders:
     try:
-        onefile = glob.glob(f'{folder}*.stm', recursive=True)[0]
+        onefile = glob.glob(f'{folder}*sm*.stm', recursive=True)[0]
     except IndexError: # list index out of range -> no .stm files in this directory
         continue # skip this directory
+    print(folder)
     with open(onefile, 'r') as f:
         lines = f.readlines()
     firstline = lines[0]
@@ -41,6 +48,20 @@ for folder in station_folders:
     df.lon[i] = lon
     df.start[i] = station_start
     df.end[i] = station_end
+
+    # get metadata
+    try:
+        infofile = glob.glob(f'{folder}*static_variables.csv')[0]
+    except IndexError: # file does not exist
+        df.land_cover_class[i] = 'NaN'
+    else:
+        info = pd.read_csv(infofile, delimiter=';')
+        try:
+            df.land_cover_class[i] = str(info[info.quantity_name == 'land cover classification'].description.iloc[-1])
+        except AttributeError: # column names are different
+            df.land_cover_class[i] = 'NaN'
+
+    # counter
     i += 1
 
 # interpolate station locations on era5 grid
@@ -70,6 +91,19 @@ for lat, lon in zip(station_grid_lat,station_grid_lon):
     klon = find_closest(koeppen.lon.values, lon)
     koeppen_class.append(koeppen.sel(lon=klon, lat=klat).values.item())
 df['koeppen_class'] = koeppen_class
+
+# calculate reduced koeppen classes
+print('get simplified koeppen class of station location')
+k_reduced = [0,1,2,3,4,4,5,5,6,6,6,7,7,7,8,8,8,9,9,9,9,10,10,10,10,11,11,11,11,12,13]
+kdict = dict(zip(range(31),k_reduced))
+stations_koeppen_class = []
+for s, station in df.iterrows():
+    stations_koeppen_class.append(kdict[station.koeppen_class])
+df['simplified_koeppen_class'] = stations_koeppen_class
+
+# remove stations that are in ocean koeppen class
+df = df[df.koeppen_class != 0]
+
+# save
 print(df.head())
 df.to_csv(f'{largefilepath}station_info_grid.csv')
-
