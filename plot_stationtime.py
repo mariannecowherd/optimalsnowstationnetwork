b4b5a286073d7a20ce3c7fbc02de061133cf90b9
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2021-07-02 10:05:24 +0200

    stations sorted after country

--- plot_stationtime.py
+++ plot_stationtime.py
@@ -7,6 +7,7 @@ import cartopy.crs as ccrs
 # open data map
 largefilepath = '/net/so4/landclim/bverena/large_files/'
 data = xr.open_dataarray(f'{largefilepath}df_gaps.nc')
+data = data.sortby('country')
 
 # calculate anomalies
 mean = data.mean(dim='time')
@@ -14,5 +15,19 @@ std = data.std(dim='time')
 data = (data - mean) / std
 
 # plot
-data.plot(vmin=-2, vmax=2, cmap='coolwarm_r')
+fig = plt.figure()
+ax = fig.add_subplot(111)
+fig.suptitle('monthly aggregated soil moisture standardised anomalies for all ISMN stations')
+ax.imshow(np.flipud(data.values), vmin=-2, vmax=2, cmap='coolwarm_r')
+
+# set ticklabels
+xticks = [0] + np.cumsum(np.unique(data.country.values, return_counts=True)[1])[:-1].tolist()
+xticklabels = np.unique(data.country.values)
+yticks = np.arange(0,504,12*5)
+yticklabels = np.arange(1979,2021,5)[::-1]
+ax.set_xticks(xticks)
+ax.set_yticks(yticks)
+ax.set_xticklabels(xticklabels, rotation=90)
+ax.set_yticklabels(yticklabels)
+#data.plot(ax=ax, vmin=-2, vmax=2, cmap='coolwarm_r')
 plt.show()
