e96a43dddc289ec211c99864bf22fa833916fc59
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2021-07-05 14:22:05 +0200

    aggregate to continents

--- plot_stationtime.py
+++ plot_stationtime.py
@@ -4,10 +4,36 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 
+# TODO add QF removed values as grey points?
+
 # open data map
 largefilepath = '/net/so4/landclim/bverena/large_files/'
 data = xr.open_dataarray(f'{largefilepath}df_gaps.nc')
 data = data.sortby('country')
+
+# simplify some countries to continents for less labels on x axis
+countrydict = {'Alaska': 'USA',
+               'Austria': 'Europe',
+               'Denmark': 'Europe',
+               'Finland': 'Europe',
+               'France': 'Europe',
+               'Germany': 'Europe',
+               'Italy': 'Europe',
+               'Poland': 'Europe',
+               'Romania': 'Europe',
+               'Spain': 'Europe',
+               'UK': 'Europe',
+               'Benin,Niger,Mali': 'Africa',
+               'CotedIvoire,Nigeria,Ghana,Uganda,Rwanda,Kenya': 'Africa',
+               'Senegal': 'Africa'}
+continent = []
+for c in data.country:
+    try:
+        continent.append(countrydict[c.item()])
+    except KeyError:
+        continent.append(c.item())
+data = data.assign_coords(continent=('stations',continent))
+data = data.sortby('continent')
 
 # calculate anomalies
 mean = data.mean(dim='time')
@@ -21,8 +47,8 @@ fig.suptitle('monthly aggregated soil moisture standardised anomalies for all IS
 ax.imshow(np.flipud(data.values), vmin=-2, vmax=2, cmap='coolwarm_r')
 
 # set ticklabels
-xticks = [0] + np.cumsum(np.unique(data.country.values, return_counts=True)[1])[:-1].tolist()
-xticklabels = np.unique(data.country.values)
+xticks = [0] + np.cumsum(np.unique(data.continent.values, return_counts=True)[1])[:-1].tolist()
+xticklabels = np.unique(data.continent.values)
 yticks = np.arange(0,504,12*5)
 yticklabels = np.arange(1979,2021,5)[::-1]
 ax.set_xticks(xticks)
