cc6c9ff4a598e66ca93e50512e0616b5f4aed0e6
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2021-07-20 16:23:45 +0200

    add predictors, aggregated function for file readin

--- upscale_cmip.py
+++ upscale_cmip.py
@@ -13,7 +13,9 @@ from sklearn.ensemble import RandomForestRegressor
 # read CMIP6 files
 varnames_predictors = ['tas','tasmax','pr','hfls','rsds']
 varname_predictand = 'mrso'
+
 def rename_vars(data):
+    varname = list(data.keys())[0]
     _, _, modelname, _, ensemblename, _ = data.encoding['source'].split('_')
     data = data.rename({varname: f'{modelname}_{ensemblename}'})
     try:
@@ -23,36 +25,63 @@ def rename_vars(data):
     #data = data.drop_dims('height')
     if isinstance(data.time[0].item(), cftime._cftime.DatetimeNoLeap):
         data['time'] = data.indexes['time'].to_datetimeindex()
+    if isinstance(data.time[0].item(), cftime._cftime.Datetime360Day):
+        data['time'] = data.indexes['time'].to_datetimeindex()
     return data
 
-sm_path = '/net/atmos/data/cmip6-ng/mrso/mon/g025/'
-largefilepath = '/net/so4/landclim/bverena/large_files/'
-varname = 'mrso'
-mrso = xr.open_mfdataset(f'{sm_path}{varname}*_historical_r1i1p1f1_*.nc', # shoyer says better use instead of glob
-                         combine='nested', # timestamp problem
-                         concat_dim=None,  # timestamp problem
-                         preprocess=rename_vars)  # rename vars to model name
-mrso = mrso.to_array()
-mrso.coords['lon'] = (mrso.coords['lon'] + 180) % 360 - 180
-mrso = mrso.sortby('lon')
+def open_cmip_suite(varname, experimentname, ensemblename):
+    cmip6_path = f'/net/atmos/data/cmip6-ng/{varname}/mon/g025/'
+    data = xr.open_mfdataset(f'{cmip6_path}{varname}*_{experimentname}_{ensemblename}_*.nc', 
+                             combine='nested', # timestamp problem
+                             concat_dim=None,  # timestamp problem
+                             preprocess=rename_vars)  # rename vars to model name
+    data = data.to_array().rename(varname)
+    data.coords['lon'] = (data.coords['lon'] + 180) % 360 - 180
+    data = data.sortby('lon')
+    data = data.mean(dim='variable').load() # here for now
+
+    return data
+
+experimentname = 'historical'
+ensemblename = 'r1i1p1f1'
+mrso = open_cmip_suite('mrso', experimentname, ensemblename)
+tasmax = open_cmip_suite('tasmax', experimentname, ensemblename)
+tas = open_cmip_suite('tas', experimentname, ensemblename)
+pr = open_cmip_suite('pr', experimentname, ensemblename)
+hfls = open_cmip_suite('hfls', experimentname, ensemblename)
+rsds = open_cmip_suite('rsds', experimentname, ensemblename)
+
+# merge predictors into one dataset 
+pred = xr.merge([tasmax,tas,pr,hfls,rsds])
+
+# select timerange of ismn
+mrso = mrso.sel(time=slice('1960','2014'))
+pred = pred.sel(time=slice('1960','2014'))
+
+# not sure why this is necessary # TODO
+mrso = mrso.resample(time='1M').mean()
+pred = pred.resample(time='1M').mean()
 
 # read station data
+largefilepath = '/net/so4/landclim/bverena/large_files/'
 df_gaps = xr.open_dataset(f'{largefilepath}df_gaps.nc')
 df_gaps = df_gaps['__xarray_dataarray_variable__']
 
-# calculate sm anomaly 
+# calculate deseasonalised anomaly 
 seasonal_mean = mrso.groupby('time.month').mean()
 seasonal_std = mrso.groupby('time.month').std()
 mrso = (mrso.groupby('time.month') - seasonal_mean) 
 mrso = mrso.groupby('time.month') / seasonal_std
 
+seasonal_mean = pred.groupby('time.month').mean()
+seasonal_std = pred.groupby('time.month').std()
+pred = (pred.groupby('time.month') - seasonal_mean) 
+pred = pred.groupby('time.month') / seasonal_std
+
 seasonal_mean = df_gaps.groupby('time.month').mean()
 seasonal_std = df_gaps.groupby('time.month').std()
 df_gaps = (df_gaps.groupby('time.month') - seasonal_mean) 
 df_gaps = df_gaps.groupby('time.month') / seasonal_std
-
-# select timerange of ismn
-mrso = mrso.sel(time=slice('1960','2014'))
 
 # regrid station data to CMIP6 grid
 lat_cmip = []
@@ -65,9 +94,7 @@ for lat, lon in zip(df_gaps.lat, df_gaps.lon):
 #df_gaps['lon_cmip'] = lon_cmip
 latlon_unique = np.unique(np.array([lat_cmip, lon_cmip]), axis=1)
 
-# calculate MMM here for now
-mrso = mrso.mean(dim='variable')
-
+import IPython; IPython.embed()
 # use xoak to select gridpoints from trajectory
 import xoak
 lat_mesh, lon_mesh = np.meshgrid(mrso.lat, mrso.lon) # coords need to be in mesh format
