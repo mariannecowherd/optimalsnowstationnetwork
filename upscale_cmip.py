aae1720a4ce3b44327b33349d4b00ba83c031ad8
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2021-07-21 10:27:05 +0200

    define X and y for training, loop over years

--- upscale_cmip.py
+++ upscale_cmip.py
@@ -66,6 +66,7 @@ pred = pred.resample(time='1M').mean()
 largefilepath = '/net/so4/landclim/bverena/large_files/'
 stations = xr.open_dataset(f'{largefilepath}df_gaps.nc')
 stations = stations['__xarray_dataarray_variable__']
+stations = stations.sel(time=slice('1960','2014'))
 
 # calculate deseasonalised anomaly 
 seasonal_mean = mrso.groupby('time.month').mean()
@@ -97,15 +98,28 @@ latlon_unique = np.unique(np.array([lat_cmip, lon_cmip]), axis=1)
 gridpoints = xr.DataArray(np.zeros((stations.shape[0],latlon_unique.shape[1])),
                           dims=['time','latlon'],
                           coords=[stations.time, np.arange(508)])
+lat_mesh, lon_mesh = np.meshgrid(mrso.lat, mrso.lon) # coords need to be in mesh format
 for i in range(latlon_unique.shape[1]):
     lat, lon = latlon_unique[:,i]
     gridpoint_mean = stations.where((stations.lat_cmip == lat) & (stations.lon_cmip == lon), drop=True).mean(dim='stations')
     gridpoints.loc[:,i] = gridpoint_mean
-import IPython; IPython.embed()
+    mask = ((lat_mesh == lat) & (lon_mesh == lon))
+    lat_mesh[mask] = np.nan
+    lon_mesh[mask] = np.nan
+
 gridpoints = gridpoints.assign_coords(lat_cmip=('latlon',latlon_unique[0,:]))
 gridpoints = gridpoints.assign_coords(lon_cmip=('latlon',latlon_unique[1,:]))
 
-import IPython; IPython.embed()
+lat_mesh = lat_mesh[~np.isnan(lat_mesh)]
+lon_mesh = lon_mesh[~np.isnan(lon_mesh)]
+latlon_unobs = np.array([lat_mesh, lon_mesh])
+arr_unobs = xr.DataArray(np.zeros((latlon_unobs.shape[1])),
+                         dims = ['latlon'],
+                         coords = [np.arange(9860)])
+arr_unobs = arr_unobs.assign_coords(lat_cmip=('latlon',latlon_unobs[0,:]))
+arr_unobs = arr_unobs.assign_coords(lon_cmip=('latlon',latlon_unobs[1,:]))
+
+
 # use xoak to select gridpoints from trajectory
 import xoak
 lat_mesh, lon_mesh = np.meshgrid(mrso.lat, mrso.lon) # coords need to be in mesh format
@@ -113,3 +127,39 @@ mrso['lat_mesh'] = (('lon','lat'), lat_mesh)
 mrso['lon_mesh'] = (('lon','lat'), lon_mesh)
 mrso.xoak.set_index(['lat_mesh', 'lon_mesh'], 'sklearn_geo_balltree')
 mrso_obs = mrso.xoak.sel(lat_mesh=gridpoints.lat_cmip, lon_mesh=gridpoints.lon_cmip)
+
+pred = pred.assign_coords(lat_mesh=(('lon','lat'), lat_mesh))
+pred = pred.assign_coords(lon_mesh=(('lon','lat'), lon_mesh))
+pred.xoak.set_index(['lat_mesh', 'lon_mesh'], 'sklearn_geo_balltree')
+pred_obs = pred.xoak.sel(lat_mesh=gridpoints.lat_cmip, lon_mesh=gridpoints.lon_cmip)
+
+pred_unobs = pred.xoak.sel(lat_mesh=arr_unobs.lat_cmip, lon_mesh=arr_unobs.lon_cmip)
+
+# flatten to skikit-learn digestable table
+X_train = pred_obs.stack(obspoints=('latlon','time')).to_array().T
+y_train = mrso_obs.stack(obspoints=('latlon','time'))
+
+X_test = pred_unobs.stack(obspoints=('latlon','time')).to_array().T
+
+# rf settings TODO later use GP
+n_trees = 100
+kwargs = {'n_estimators': n_trees,
+          'min_samples_leaf': 2,
+          'max_features': 'auto', 
+          'max_samples': None, 
+          'bootstrap': True,
+          'warm_start': True,
+          'n_jobs': None, # set to number of trees
+          'verbose': 0}
+
+import IPython; IPython.embed()
+# loop over years
+for year in np.arange(1976,2015):
+    next_year = str(year + 1)
+    year = str(year)
+    
+    rf = RandomForestRegressor(**kwargs)
+    rf.fit(X_train.sel(time=slice('1960',year)), 
+           y_train.sel(time=slice('1960',year)))
+
+    rf.predict(X_test.sel(time=slice(year, next_year))) 
