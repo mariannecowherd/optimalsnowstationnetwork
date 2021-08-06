294570b8a7d7e29c84d86435c637f30eb2098c4e
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2021-08-06 15:36:44 +0200

    plot drought frequency and duration

--- calc_drought_indices.py
+++ calc_drought_indices.py
@@ -2,6 +2,7 @@
 TEST
 """
 
+import regionmask
 import numpy as np
 import pandas as pd
 import xarray as xr
@@ -16,25 +17,54 @@ ensemblename = 'r1i1p1f1'
 orig = xr.open_dataset(f'{largefilepath}mrso_orig_{modelname}_{experimentname}_{ensemblename}.nc')['mrso']
 pred = xr.open_dataset(f'{largefilepath}mrso_pred_{modelname}_{experimentname}_{ensemblename}.nc')['mrso']
 
-# drought indices: duration, severity, frequency, extent 
-perc90 = orig.quantile(0.1, dim='time')
-orig_drought = (orig - perc90)
+# define drought as driest 10% of values for each of the datasets individually
+orig_perc = orig.quantile(0.1, dim='time')
+orig_drought = (orig - orig_perc)
 orig_drought = orig_drought.where(orig_drought < 0, 0)
-perc90 = pred.quantile(0.1, dim='time') # two options, this one is harder, show that not only the maginutide is off (same percentiles) but also some droughts are missed outside of well-observed spaces (different percentiles)
-pred_drought = (pred - perc90)
+
+pred_perc = pred.quantile(0.1, dim='time') # two options, this one is harder, show that not only the maginutide is off (same percentiles) but also some droughts are missed outside of well-observed spaces (different percentiles)
+pred_drought = (pred - pred_perc)
 pred_drought = pred_drought.where(pred_drought < 0, 0)
 
-# plot 
+# drought indices: duration, severity, frequency, extent 
+# TODO also do severity and duration, but for now only frequency and extent
+pred_bool = (pred_drought < 0)*1 # True (1) is drought
+orig_bool = (orig_drought < 0)*1 # True (1) is drought
+
 proj = ccrs.PlateCarree()
-for t, time in enumerate(pred_drought.time):
-    fig = plt.figure(figsize=(10,3))
-    fig.suptitle(f'{str(time.values)[:7]}')
-    ax1 = fig.add_subplot(121, projection=proj)
-    ax2 = fig.add_subplot(122, projection=proj)
-    orig_drought.sel(time=time).plot(ax=ax1, cmap='hot', add_colorbar=False, vmin=-2, vmax=0)
-    pred_drought.sel(time=time).plot(ax=ax2, cmap='hot', add_colorbar=False, vmin=-2, vmax=0)
-    ax1.set_title('orig')
-    ax2.set_title('upscaled')
-    ax1.coastlines()
-    ax2.coastlines()
-    plt.savefig(f'drought_gif/drought_evolution_{t:03d}.png')
+fig = plt.figure(figsize=(10,3))
+ax1 = fig.add_subplot(121, projection=proj)
+ax2 = fig.add_subplot(122, projection=proj)
+ax1.coastlines()
+ax2.coastlines()
+(orig_bool.diff(dim='time') == 1).sum(dim='time').plot(ax=ax1, vmin=0, vmax=60, cmap='hot_r')
+(pred_bool.diff(dim='time') == 1).sum(dim='time').plot(ax=ax2, vmin=0, vmax=60, cmap='hot_r')
+plt.show()
+
+
+# look at drought pattern per region
+#mask = regionmask.defined_regions.ar6.all.mask(orig)
+#res = xr.full_like(mask, np.nan)
+##orig_freq = orig_drought != 0
+##pred_freq = pred_drought != 0
+##drought_underestimation = (orig_freq.groupby('time.year').sum() - pred_freq.groupby('time.year').sum()) # underestimation is positive
+#for i in range(58):
+#    tmp = orig_drought.where(mask == i).mean(dim=('lat','lon')) - pred_drought.where(mask == i).mean(dim=('lat','lon'))
+#    res = res.where(mask != i, tmp.sum().item())
+#
+#import IPython; IPython.embed()
+#
+## plot 
+#proj = ccrs.PlateCarree()
+#for t, time in enumerate(pred_drought.time):
+#    fig = plt.figure(figsize=(10,3))
+#    fig.suptitle(f'{str(time.values)[:7]}')
+#    ax1 = fig.add_subplot(121, projection=proj)
+#    ax2 = fig.add_subplot(122, projection=proj)
+#    orig_drought.sel(time=time).plot(ax=ax1, cmap='hot', add_colorbar=False, vmin=-2, vmax=0)
+#    pred_drought.sel(time=time).plot(ax=ax2, cmap='hot', add_colorbar=False, vmin=-2, vmax=0)
+#    ax1.set_title('orig')
+#    ax2.set_title('upscaled')
+#    ax1.coastlines()
+#    ax2.coastlines()
+#    plt.savefig(f'drought_gif/drought_evolution_{t:03d}.png')
