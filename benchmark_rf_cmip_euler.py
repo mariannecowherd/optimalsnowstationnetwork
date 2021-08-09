7cf0c6c8e3c9e6e2e1c733c40947787ca7b11d6e
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2021-08-09 11:20:04 +0200

    debug data preproc

--- benchmark_rf_cmip_euler.py
+++ benchmark_rf_cmip_euler.py
@@ -13,8 +13,12 @@ from sklearn.ensemble import RandomForestRegressor
 
 # load feature tables
 largefilepath = '/cluster/work/climate/bverena/'
-mrso_land = xr.open_dataset(f'{largefilepath}mrso_land.nc')
-pred_land = xr.open_dataset(f'{largefilepath}pred_land.nc')
+largefilepath = '/net/so4/landclim/bverena/large_files/'
+mrso_land = xr.open_dataset(f'{largefilepath}mrso_land.nc')['mrso'].load()
+pred_land = xr.open_dataset(f'{largefilepath}pred_land.nc')['__xarray_dataarray_variable__'].load()
+
+mrso_land = mrso_land.set_index(datapoints=('landpoints','time'))
+pred_land = pred_land.set_index(datapoints=('landpoints','time'))
 
 # rf settings TODO later use GP
 kwargs = {'n_estimators': 100, # TODO 100 this is debug
@@ -25,12 +29,15 @@ kwargs = {'n_estimators': 100, # TODO 100 this is debug
           'warm_start': False,
           'n_jobs': 50, # set to number of trees
           'verbose': 0}
-import IPython; IPython.embed()
 
 #mrso_pred = xr.full_like(mrso, np.nan)
 largefilepath = '/net/so4/landclim/bverena/large_files/'
 from os.path import exists
 #mrso_pred = xr.open_dataset(f'{largefilepath}mrso_benchmark_{modelname}_{experimentname}_{ensemblename}.nc')['mrso']
+landpoints = np.unique(mrso_land.landpoints)
+modelname = 'CanESM5'
+experimentname = 'historical'
+ensemblename = 'r1i1p1f1'
 
 for g, gridpoint in enumerate(landpoints): # random folds of observed gridpoints # LG says doesnot matter if random or regionally grouped, both has advantages and disadvantages, just do something and reason why
 
@@ -45,6 +52,7 @@ for g, gridpoint in enumerate(landpoints): # random folds of observed gridpoints
     #    print(f'gridpoint {g} is not yet computed, continue')
     if exists(f'{largefilepath}mrso_benchmark_{g}_{modelname}_{experimentname}_{ensemblename}.nc'):
         print(f'gridpoint {g} is already computed, skip')
+        continue
     else:
         print(f'gridpoint {g} is not yet computed, continue')
         
