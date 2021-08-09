f2a6fb419058d3bd739f622590acf87c1ead263c
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2021-08-09 15:46:06 +0200

    save logfiles to specific folder

--- run_lowres.sh
+++ run_lowres.sh
@@ -3,7 +3,7 @@
 #set -x
 # mem in MB per core
 #epoch=36
-for g in $(seq 0 10); do
-    bsub -R "rusage[mem=300]" -eo log_g${g}.out -oo log_g${g}.out -n 100 -W 00:10 python benchmark_rf_cmip_euler.py -g ${g}
+for g in $(seq 0 100); do
+    bsub -R "rusage[mem=300]" -eo logfiles/log_g${g}.out -oo logfiles/log_g${g}.out -n 100 -W 00:10 python benchmark_rf_cmip_euler.py -g ${g}
     echo "${g} subitted to queue ..."
 done
