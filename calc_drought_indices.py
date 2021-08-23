a8f4dbc6e4919d8a5b1bbe72b00aad0c1d3b8f93
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2021-08-23 11:43:02 +0200

    new plot rmse per koeppen

--- calc_drought_indices.py
+++ calc_drought_indices.py
@@ -91,6 +91,11 @@ plt.show()
 orig_sum = orig_drought.sum(dim='time')
 pred_sum = pred_drought.sum(dim='time')
 benchmark_sum = benchmark_drought.sum(dim='time')
+
+mae_irreducible = np.abs(orig_sum - benchmark_sum)
+mae_represent = np.abs(orig_sum - pred_sum)
+mae_diff = mae_represent - mae_irreducible
+
 proj = ccrs.PlateCarree()
 fig = plt.figure(figsize=(10,3))
 fig.suptitle('global distribution of 10% driest values, mean per gridcell')
@@ -100,14 +105,72 @@ ax3 = fig.add_subplot(133, projection=proj)
 ax1.coastlines()
 ax2.coastlines()
 ax3.coastlines()
-vmin= -50
+vmin= 0
 vmax= 50
-(orig_sum - benchmark_sum).plot(ax=ax1, cmap='coolwarm_r', vmin=vmin, vmax=vmax, add_colorbar=True, cbar_kwargs={'orientation': 'horizontal', 'label': 'mean standardised soil moisture anomaly during drought'})
-(orig_sum - pred_sum).plot(ax=ax2, cmap='coolwarm_r', vmin=vmin, vmax=vmax, add_colorbar=False)
-((orig_sum - pred_sum) - (orig_sum - benchmark_sum)).plot(ax=ax3, cmap='coolwarm', add_colorbar=True, cbar_kwargs={'orientation': 'horizontal'})
-ax1.set_title('orig - benchmark')
-ax2.set_title('orig - pred')
+mae_irreducible.plot(ax=ax1, cmap='Reds', vmin=vmin, vmax=vmax, add_colorbar=True, cbar_kwargs={'orientation': 'horizontal', 'label': 'mean standardised soil moisture anomaly during drought'})
+mae_represent.plot(ax=ax2, cmap='Reds', vmin=vmin, vmax=vmax, add_colorbar=False)
+(mae_represent - mae_irreducible).plot(ax=ax3, cmap='coolwarm', add_colorbar=True, cbar_kwargs={'orientation': 'horizontal'})
+ax1.set_title('irreducible error')
+ax2.set_title('representation error')
+ax3.set_title('positive: large repr div')
 plt.show()
+
+# investivage mae diff
+data = xr.open_dataset(f'{largefilepath}df_gaps.nc')['__xarray_dataarray_variable__']
+lat_cmip = []
+lon_cmip = []
+for lat, lon in zip(data.lat, data.lon):
+    point = mae_diff.sel(lat=lat, lon=lon, method='nearest')
+    lat_cmip.append(point.lat.item())
+    lon_cmip.append(point.lon.item())
+data = data.assign_coords(lat_cmip=('stations',lat_cmip))
+data = data.assign_coords(lon_cmip=('stations',lon_cmip))
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection=proj)
+mae_diff.plot(ax=ax)
+ax.scatter(data.lon_cmip, data.lat_cmip, transform=proj)
+ax.coastlines()
+plt.show()
+
+# mae diff per koeppen climate
+fig = plt.figure()
+fig.suptitle('Difference between RMSE of mean soil moisture anomaly (10% driest values globally) of CMIP-U and CMIP-B')
+ax1 = fig.add_subplot(121, projection=proj)
+ax2 = fig.add_subplot(122)
+
+orig_drought = orig.where(orig < orig.quantile(0.1))
+pred_drought = pred.where(pred < pred.quantile(0.1))
+benchmark_drought = benchmark.where(benchmark < benchmark.quantile(0.1))
+
+rmse_irreducible = np.sqrt(((orig_drought - benchmark_drought)**2).mean(dim='time'))
+rmse_repr = np.sqrt(((orig_drought - pred_drought)**2).mean(dim='time'))
+rmse_diff = rmse_repr - rmse_irreducible
+rmse_diff.plot(ax=ax1, cbar_kwargs={'orientation': 'horizontal', 'label': 'RMSE difference'})
+ax1.coastlines()
+ax1.set_title('')
+
+koeppen_simple = xr.open_dataset(f'{largefilepath}koeppen_simple.nc')['__xarray_dataarray_variable__']
+import xesmf as xe
+regridder = xe.Regridder(mae_diff, koeppen_simple, 'bilinear', reuse_weights=False)
+koeppen_names = ['Af','Am','Aw','BW','BS','Cs','Cw','Cf','Ds','Dw','Df','ET','EF']
+rmse_diff = regridder(rmse_diff)
+koeppen_rmse = []
+n_koeppen = list(range(1,14))
+koeppen_density = [3.3126, 2.4572, 3.5573, 9.6494, 23.995, 117.3616, 2.8483, 41.2927, 60.6654, 29.6423, 37.4495, 8.3342, 0.2127]
+for k in n_koeppen:
+    mae_tmp = rmse_diff.where(koeppen_simple == k, np.nan)
+    koeppen_rmse.append(mae_tmp.mean().item())
+#fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(5,5))
+#fig.suptitle('koeppen climate classes')
+ax2.scatter(koeppen_density, koeppen_rmse, c='blue')
+for i,(x,y) in enumerate(zip(koeppen_density,koeppen_rmse)):
+    ax2.annotate(koeppen_names[i], xy=(x,y))
+ax2.set_ylabel('RMSE difference')
+ax2.set_xlabel('station density [bio km^2]')
+ax2.set_title('Per Koeppen climate')
+plt.show()
+import IPython; IPython.embed()
 
 
 
