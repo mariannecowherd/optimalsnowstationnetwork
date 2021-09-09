592cd8b648536cad40657f7deefae2641e7ca387
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2021-09-09 09:58:15 +0200

    use new stations cmip data, debug

--- upscale_change_cmip.py
+++ upscale_change_cmip.py
@@ -13,6 +13,9 @@ import cartopy.crs as ccrs
 from sklearn.ensemble import RandomForestRegressor
 import argparse
 
+# define constants
+largefilepath = '/net/so4/landclim/bverena/large_files/'
+
 # read gridpoint info
 #parser = argparse.ArgumentParser()
 #parser.add_argument('--lat', dest='lat', type=int)
@@ -20,111 +23,34 @@ import argparse
 #args = parser.parse_args()
 #gridpoint = args.g
 
-# read CMIP6 files
-def rename_vars(data):
-    varname = list(data.keys())[0]
-    _, _, modelname, _, ensemblename, _ = data.encoding['source'].split('_')
-    data = data.rename({varname: f'{modelname}_{ensemblename}'})
-    try:
-        data = data.drop_vars('file_qf')
-    except ValueError:
-        pass
-    #data = data.drop_dims('height')
-    if isinstance(data.time[0].item(), cftime._cftime.DatetimeNoLeap): # TODO add in again once several models used
-        data['time'] = data.indexes['time'].to_datetimeindex()
-    if isinstance(data.time[0].item(), cftime._cftime.Datetime360Day):
-        data['time'] = data.indexes['time'].to_datetimeindex()
-    return data
-
-def open_cmip_suite(varname, modelname, experimentname, ensemblename):
-    cmip6_path = f'/net/atmos/data/cmip6-ng/{varname}/mon/g025/'
-    data = xr.open_mfdataset(f'{cmip6_path}{varname}*_{modelname}_{experimentname}_{ensemblename}_*.nc', 
-                             combine='nested', # timestamp problem
-                             concat_dim=None,  # timestamp problem
-                             preprocess=rename_vars)  # rename vars to model name
-    data = data.to_array().rename(varname)
-    data.coords['lon'] = (data.coords['lon'] + 180) % 360 - 180
-    data = data.sortby('lon')
-    data = data.mean(dim='variable').load() # here for now
-
-    return data
-
+# TODO include in preproc
 modelname = 'CanESM5'
 experimentname = 'historical'
 ensemblename = 'r1i1p1f1'
-mrso_hist = open_cmip_suite('mrso', modelname, experimentname, ensemblename)
-tas_hist = open_cmip_suite('tas', modelname, experimentname, ensemblename)
-pr_hist = open_cmip_suite('pr', modelname, experimentname, ensemblename)
-modelname = 'CanESM5'
-experimentname = 'ssp585'
-ensemblename = 'r1i1p1f1'
-mrso_ssp = open_cmip_suite('mrso', modelname, experimentname, ensemblename)
-tas_ssp = open_cmip_suite('tas', modelname, experimentname, ensemblename)
-pr_ssp = open_cmip_suite('pr', modelname, experimentname, ensemblename)
+
+# read CMIP6 files
+mrso_hist = xr.open_dataset(f'{largefilepath}mrso_historical.nc')
+mrso_fut = xr.open_dataset(f'{largefilepath}mrso_ssp585.nc')
+pred_hist = xr.open_dataset(f'{largefilepath}pred_historical.nc')
+pred_fut = xr.open_dataset(f'{largefilepath}pred_ssp585.nc')
 
 # merge historical and future scenarios
-mrso = xr.concat([mrso_hist, mrso_ssp ], dim='time')
-tas = xr.concat([tas_hist, tas_ssp ], dim='time')
-pr = xr.concat([pr_hist, pr_ssp ], dim='time')
+mrso = xr.concat([mrso_hist, mrso_fut], dim='time')
+pred = xr.concat([pred_hist, pred_fut], dim='time')
 
-# cut out Greenland and Antarctica
-n_greenland = regionmask.defined_regions.natural_earth.countries_110.map_keys('Greenland')
-n_antarctica = regionmask.defined_regions.natural_earth.countries_110.map_keys('Antarctica')
-mask = regionmask.defined_regions.natural_earth.countries_110.mask(mrso)
-mrso = mrso.where((mask != n_greenland) & (mask != n_antarctica) & (~np.isnan(mask)))
-
-# create lagged features
-tas_1month = tas.copy(deep=True).shift(time=1, fill_value=0).rename('tas_1m')
-tas_2month = tas.copy(deep=True).shift(time=2, fill_value=0).rename('tas_2m')
-tas_3month = tas.copy(deep=True).shift(time=3, fill_value=0).rename('tas_3m')
-tas_4month = tas.copy(deep=True).shift(time=4, fill_value=0).rename('tas_4m')
-tas_5month = tas.copy(deep=True).shift(time=5, fill_value=0).rename('tas_5m')
-tas_6month = tas.copy(deep=True).shift(time=6, fill_value=0).rename('tas_6m')
-tas_7month = tas.copy(deep=True).shift(time=7, fill_value=0).rename('tas_7m')
-tas_8month = tas.copy(deep=True).shift(time=8, fill_value=0).rename('tas_8m')
-tas_9month = tas.copy(deep=True).shift(time=9, fill_value=0).rename('tas_9m')
-tas_10month = tas.copy(deep=True).shift(time=10, fill_value=0).rename('tas_10m')
-tas_11month = tas.copy(deep=True).shift(time=11, fill_value=0).rename('tas_11m')
-tas_12month = tas.copy(deep=True).shift(time=12, fill_value=0).rename('tas_12m')
-
-pr_1month = pr.copy(deep=True).shift(time=1, fill_value=0).rename('pr_1m') 
-pr_2month = pr.copy(deep=True).shift(time=2, fill_value=0).rename('pr_2m')
-pr_3month = pr.copy(deep=True).shift(time=3, fill_value=0).rename('pr_3m')
-pr_4month = pr.copy(deep=True).shift(time=4, fill_value=0).rename('pr_4m')
-pr_5month = pr.copy(deep=True).shift(time=5, fill_value=0).rename('pr_5m')
-pr_6month = pr.copy(deep=True).shift(time=6, fill_value=0).rename('pr_6m')
-pr_7month = pr.copy(deep=True).shift(time=7, fill_value=0).rename('pr_7m')
-pr_8month = pr.copy(deep=True).shift(time=8, fill_value=0).rename('pr_8m')
-pr_9month = pr.copy(deep=True).shift(time=9, fill_value=0).rename('pr_9m')
-pr_10month = pr.copy(deep=True).shift(time=10, fill_value=0).rename('pr_10m')
-pr_11month = pr.copy(deep=True).shift(time=11, fill_value=0).rename('pr_11m')
-pr_12month = pr.copy(deep=True).shift(time=12, fill_value=0).rename('pr_12m')
-
-# merge predictors into one dataset 
-pred = xr.merge([tas, tas_1month, tas_2month, tas_3month, tas_4month, tas_5month, tas_6month,
-                 tas_7month, tas_8month, tas_9month, tas_10month, tas_11month, tas_12month,
-                 pr, pr_1month, pr_2month, pr_3month, pr_4month, pr_5month, pr_6month,
-                 pr_7month, pr_8month, pr_9month, pr_10month, pr_11month, pr_12month])
-
-import IPython; IPython.embed()
 # select timerange of ismn
-mrso = mrso.sel(time=slice('1960','2030'))
-pred = pred.sel(time=slice('1960','2030'))
-
-# not sure why this is necessary # TODO
-#mrso = mrso.resample(time='1M').mean()
-#pred = pred.resample(time='1M').mean()
+mrso = mrso.sel(time=slice('1960','2050'))
+pred = pred.sel(time=slice('1960','2050'))
 
 # read station data
-largefilepath = '/net/so4/landclim/bverena/large_files/'
-stations = xr.open_dataset(f'{largefilepath}df_gaps.nc')
-stations = stations['__xarray_dataarray_variable__']
+stations = xr.open_dataset(f'{largefilepath}df_gaps_cmip.nc')['mrso']
 
 # select only "still existing" stations in future
 iold = np.isnan(stations.sel(time=slice('2015','2020'))).all(axis=0)
 stations.loc[slice('2015','2020'),~iold] = 1
-tmp = xr.full_like(stations.sel(time=slice('2010','2019')), np.nan)
-timerange = pd.date_range('2021-01-01','2031-01-01', freq='M')
+timerange = pd.date_range('2021-01-01','2051-01-01', freq='M')
+tmp = stations[:len(timerange),:]
+tmp[:,:] = np.nan
 tmp['time'] = timerange
 tmp[:,~iold] = 1
 stations = xr.concat([stations, tmp], dim='time')
@@ -146,62 +72,33 @@ stations = (stations.groupby('time.month') - seasonal_mean)
 stations = stations.groupby('time.month') / seasonal_std
 
 # regrid station data to CMIP6 grid
-landmask = ~np.isnan(mrso[0,:,:]).copy(deep=True)
+landmask = xr.open_dataset(f'{largefilepath}landmask_cmip6-ng.nc')['mrso'].squeeze()
 obsmask = xr.full_like(landmask, False)
 unobsmask = landmask.copy(deep=True)
-lat_cmip = []
-lon_cmip = []
-latlon_cmip = []
-for lat, lon in zip(stations.lat, stations.lon):
-    point = landmask.sel(lat=lat, lon=lon, method='nearest')
-
-    unobsmask.loc[point.lat, point.lon] = False
-
-    lat_cmip.append(point.lat.item())
-    lon_cmip.append(point.lon.item())
-    
-    if landmask.loc[point.lat,point.lon].item(): # obs gridpoint if station contained and on CMIP land
-        obsmask.loc[point.lat, point.lon] = True
-        latlon_cmip.append(f'{point.lat.item()} {point.lon.item()}')
-    else:
-        latlon_cmip.append('ocean')
-
-stations = stations.assign_coords(lat_cmip=('stations',lat_cmip))
-stations = stations.assign_coords(lon_cmip=('stations',lon_cmip))
-stations_copy = stations.copy(deep=True)
-stations = stations.assign_coords(latlon_cmip=('stations',latlon_cmip))
-stations = stations.groupby('latlon_cmip').mean()
-stations = stations.drop_sel(latlon_cmip='ocean')
-
-# add lat and lon again to grouped station data
-lat_cmip = []
-lon_cmip = []
-for latlon in stations.latlon_cmip:
-    lat, lon = latlon.item().split()
-    lat, lon = float(lat), float(lon)
-    lat_cmip.append(lat)
-    lon_cmip.append(lon)
-stations = stations.assign_coords(lat_cmip=('latlon_cmip',lat_cmip))
-stations = stations.assign_coords(lon_cmip=('latlon_cmip',lon_cmip))
+for lat, lon in zip(stations.lat_cmip, stations.lon_cmip):
+    unobsmask.loc[lat,lon] = False
+    #if landmask.loc[point.lat,point.lon].item(): # obs gridpoint if station contained and on CMIP land # this is ensured by preproc_ismn.py
+    obsmask.loc[lat,lon] = True
 
 # add another "theoretical" future stations TODO
-latlist = [18.75]
-lonlist = [48.75]
-for lat, lon in zip(latlist, lonlist):
-    station = xr.full_like(stations[:,0], np.nan)
-    station['lat_cmip'] = lat 
-    station['lon_cmip'] = lon
-    station['latlon_cmip'] = f'{lat} {lon}'
-    station.loc[slice('2022','2030')] = 1
-    stations = xr.concat([stations,station], dim='latlon_cmip')
-    obsmask.loc[lat, lon] = True
-
-# mask for unobserved future points given future network
-future_unobsmask = landmask.copy(deep=True)
-future_stations = stations.loc['2030-12-31',~np.isnan(stations.sel(time='2030-12-31'))]
-for lat, lon in zip(future_stations.lat_cmip, future_stations.lon_cmip):
-    future_unobsmask.loc[lat, lon] = False
-stations = stations.swap_dims({'latlon_cmip': 'lat_cmip'}).reset_index('latlon_cmip', drop=True) # remove latlon_cmip coordinate
+#latlist = [18.75]
+#lonlist = [48.75]
+#for lat, lon in zip(latlist, lonlist):
+#    station = xr.full_like(stations[:,0], np.nan)
+#    station['lat_cmip'] = lat 
+#    station['lon_cmip'] = lon
+#    station.loc[slice('2022','2030')] = 1
+#    station['latlon_cmip'] = f'{lat} {lon}'
+#    stations = xr.concat([stations,station], dim='latlon_cmip')
+#    obsmask.loc[lat, lon] = True
+##df_gaps = df_gaps.swap_dims({'latlon_cmip': 'lat_cmip'}).reset_index('latlon_cmip', drop=True) # remove latlon_cmip coordinate
+#
+## mask for unobserved future points given future network # TODO don't know why this was necessary
+#future_unobsmask = landmask.copy(deep=True)
+#future_stations = stations.loc['2030-12-31',~np.isnan(stations.sel(time='2030-12-31'))]
+#for lat, lon in zip(future_stations.lat_cmip, future_stations.lon_cmip):
+#    future_unobsmask.loc[lat, lon] = False
+#import IPython; IPython.embed()
 
 # divide into obs and unobs gridpoints
 obslat, obslon = np.where(obsmask)
@@ -210,9 +107,10 @@ obslat, obslon = xr.DataArray(obslat, dims='obspoints'), xr.DataArray(obslon, di
 mrso_obs = mrso.isel(lat=obslat, lon=obslon)
 pred_obs = pred.isel(lat=obslat, lon=obslon)
 
-unobslat, unobslon = np.where(future_unobsmask)
+#unobslat, unobslon = np.where(future_unobsmask)
+unobslat, unobslon = np.where(unobsmask)
 unobslat, unobslon = xr.DataArray(unobslat, dims='unobspoints'), xr.DataArray(unobslon, dims='unobspoints')
-#
+
 mrso_unobs = mrso.isel(lat=unobslat, lon=unobslon)
 pred_unobs = pred.isel(lat=unobslat, lon=unobslon)
 
@@ -243,18 +141,18 @@ kwargs = {'n_estimators': 100,
           'verbose': 0}
 
 # initialise array for saving results
-mrso_pred = xr.full_like(mrso.sel(time=slice('2020','2031')), np.nan)
+mrso_pred = xr.full_like(mrso.sel(time=slice('2020','2051')), np.nan)['mrso']
 
-for year in range(2020,2031):
+for year in range(2020,2051):
 
     # define test and training dataset
-    y_train = mrso_obstime.where(mrso_obstime['time.year'] <= year, drop=True)
-    X_train = pred_obstime.where(mrso_obstime['time.year'] <= year, drop=True)
+    y_train = mrso_obstime.where(mrso_obstime['time.year'] <= year, drop=True)['mrso']
+    X_train = pred_obstime.where(mrso_obstime['time.year'] <= year, drop=True)['mrso']
 
     y_test = mrso_unobs.where(mrso_unobs['time.year'] == year, drop=True)
     X_test = pred_unobs.where(pred_unobs['time.year'] == year, drop=True)
 
-    y_test = y_test.stack(datapoints=('unobspoints','time'))
+    y_test = y_test.stack(datapoints=('unobspoints','time'))['mrso']
     X_test = X_test.stack(datapoints=('unobspoints','time')).T
 
     # train and upscale
@@ -275,6 +173,9 @@ mrso = (mrso.groupby('time.month') + mrso_seasonal_mean)
 
 mrso_pred = (mrso_pred.groupby('time.month') * mrso_seasonal_std)
 mrso_pred = (mrso_pred.groupby('time.month') + mrso_seasonal_mean) 
+
+mrso = mrso['mrso']
+mrso_pred = mrso_pred['mrso']
 
 # find gridpoint with minimum performance
 corr = xr.corr(mrso, mrso_pred, dim='time')
