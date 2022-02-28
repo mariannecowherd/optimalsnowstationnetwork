e479c5be8bc9dbf92acf4591d876c548095c0451
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2022-02-28 14:13:04 +0100

    find max r2 of full model with new data

--- benchmark_rf_cmip_euler.py
+++ benchmark_rf_cmip_euler.py
@@ -6,6 +6,9 @@ import numpy as np
 import xarray as xr
 from sklearn.ensemble import RandomForestRegressor
 import argparse
+import logging
+logging.basicConfig(format='%(asctime)s - %(message)s', 
+                    datefmt='%d-%b-%y %H:%M:%S', level=logging.DEBUG)
 
 # read gridpoint info
 parser = argparse.ArgumentParser()
@@ -14,27 +17,38 @@ args = parser.parse_args()
 gridpoint = args.g
 
 # check if gridpoint is already computed
-largefilepath = '/cluster/work/climate/bverena/'
-modelname = 'CanESM5'
-experimentname = 'historical'
-ensemblename = 'r1i1p1f1'
+upscalepath = '/net/so4/landclim/bverena/large_files/opscaling/'
 
 #largefilepath = '/net/so4/landclim/bverena/large_files/'
-from os.path import exists
-if exists(f'{largefilepath}mrso_benchmark_{gridpoint}_{modelname}_{experimentname}_{ensemblename}.nc'):
-    print(f'gridpoint {gridpoint} is already computed, skip')
-    quit()
-else:
-    print(f'gridpoint {gridpoint} is not yet computed, continue')
-    
+#from os.path import exists
+#if exists(f'{largefilepath}mrso_benchmark_{gridpoint}_{modelname}_{experimentname}_{ensemblename}.nc'):
+#    print(f'gridpoint {gridpoint} is already computed, skip')
+#    quit()
+#else:
+#    print(f'gridpoint {gridpoint} is not yet computed, continue')
 
 # load feature tables
-print('load feature tables')
-mrso_land = xr.open_dataset(f'{largefilepath}mrso_land.nc')['mrso'].load()
-pred_land = xr.open_dataset(f'{largefilepath}pred_land.nc')['__xarray_dataarray_variable__'].load()
+logging.info('load feature tables')
+mrso = xr.open_dataset(f'{upscalepath}mrso_ssp585.nc')['mrso'].load()
+pred = xr.open_dataset(f'{upscalepath}pred_ssp585.nc').load()
 
-mrso_land = mrso_land.set_index(datapoints=('landpoints','time'))
-pred_land = pred_land.set_index(datapoints=('landpoints','time'))
+# select land points
+logging.info('select land points')
+landmask = ~np.isnan(mrso.sel(time='2021-08'))
+landmask.to_dataset(name='landmask').to_netcdf(f'{upscalepath}landmask_cmip6-ng.nc')
+landmask = xr.open_dataset(f'{upscalepath}landmask_cmip6-ng.nc')['landmask'].squeeze()
+landlat, landlon = np.where(landmask)
+landpoints = np.arange(len(landlat))
+landlat = xr.DataArray(landlat, dims='landpoints', coords=[landpoints]) 
+landlon = xr.DataArray(landlon, dims='landpoints', coords=[landpoints])
+
+mrso = mrso.isel(lat=landlat, lon=landlon)
+pred = pred.isel(lat=landlat, lon=landlon)
+
+# stack landpoints and time
+logging.info('stack')
+mrso = mrso.stack(datapoints=('landpoints','time'))
+pred = pred.stack(datapoints=('landpoints','time')).to_array().T
 
 # rf settings TODO later use GP
 kwargs = {'n_estimators': 100, # TODO 100 this is debug
@@ -43,25 +57,25 @@ kwargs = {'n_estimators': 100, # TODO 100 this is debug
           'max_samples': None, 
           'bootstrap': True,
           'warm_start': False,
-          'n_jobs': 100, # set to number of trees
-          'verbose': 0}
+          'n_jobs': 30, # set to number of trees
+          'verbose': 2}
 
-landpoints = np.unique(mrso_land.landpoints)
-X_test = pred_land.sel(landpoints=gridpoint)
-y_test = mrso_land.sel(landpoints=gridpoint)
-X_train = pred_land.where(pred_land.landpoints != gridpoint, drop=True)
-y_train = mrso_land.where(pred_land.landpoints != gridpoint, drop=True)
+landpoints = np.unique(mrso.landpoints)
+X_test = pred.sel(landpoints=gridpoint)
+y_test = mrso.sel(landpoints=gridpoint)
+X_train = pred.where(pred.landpoints != gridpoint, drop=True)
+y_train = mrso.where(pred.landpoints != gridpoint, drop=True)
 
-print('train')
+logging.info('train')
 rf = RandomForestRegressor(**kwargs)
 rf.fit(X_train, y_train)
 
-print('predict')
+logging.info('predict')
 y_predict = xr.full_like(y_test, np.nan)
 y_predict[:] = rf.predict(X_test)
 
 corr = xr.corr(y_test, y_predict).item()
-print(gridpoint, corr**2)
+logging.info(f'{gridpoint}, {corr**2}')
 
-print('save')
-y_predict.to_netcdf(f'{largefilepath}mrso_benchmark_{gridpoint}_{modelname}_{experimentname}_{ensemblename}.nc')
+logging.info('save')
+#y_predict.to_netcdf(f'{largefilepath}mrso_benchmark_{gridpoint}_{modelname}_{experimentname}_{ensemblename}.nc')
