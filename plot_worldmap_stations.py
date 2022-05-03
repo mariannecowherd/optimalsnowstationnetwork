496546c018b53d4b0854d4847e7b0da0b5c19bac
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2022-05-03 14:47:12 +0200

    still active networks, robinson projection

--- plot_worldmap_stations.py
+++ plot_worldmap_stations.py
@@ -11,7 +11,7 @@ import pandas as pd
 
 # open data
 largefilepath = '/net/so4/landclim/bverena/large_files/'
-data = xr.open_dataset(f'{largefilepath}df_gaps.nc')['__xarray_dataarray_variable__']
+data = xr.open_dataset(f'{largefilepath}df_gaps.nc')['mrso']
 koeppen_simple = xr.open_dataset(f'{largefilepath}koeppen_simple.nc')['__xarray_dataarray_variable__']
 
 # calculate simplified koeppen classes
@@ -36,23 +36,25 @@ koeppen_simple = xr.open_dataset(f'{largefilepath}koeppen_simple.nc')['__xarray_
 #cmap = LinearSegmentedColormap.from_list('koeppen', colors, N=len(colors))
 #import IPython; IPython.embed()
 
-# divide stations into still active and past
-ipast = np.isnan(data.loc[slice('2015','2021'),:].mean(dim='time'))
-data_hist = data[:,ipast]
-data_active = data[:,~ipast]
+# select the stations still active
+inactive_networks = ['HOBE','PBO_H20','IMA_CAN1','SWEX_POLAND','CAMPANIA',
+                     'HiWATER_EHWSN', 'METEROBS', 'UDC_SMOS', 'KHOREZM',
+                     'ICN','AACES','HSC_CEOLMACHEON','MONGOLIA','RUSWET-AGRO',
+                     'CHINA','IOWA','RUSWET-VALDAI','RUSWET-GRASS']
+data = data.where(~data.network.isin(inactive_networks), drop=True)
 
 # plot worldmap with all & still active stations on koeppen climates
 colors = ['white', 'darkgreen', 'forestgreen', 'darkseagreen', 'linen', 'tan', 'gold', 'lightcoral', 'peru', 'yellowgreen', 'olive', 'olivedrab', 'lightgrey', 'whitesmoke']
 fs = 25
 cmap = LinearSegmentedColormap.from_list('koeppen', colors, N=len(colors))
-proj = ccrs.PlateCarree()
+proj = ccrs.Robinson()
+transf = ccrs.PlateCarree()
 fig = plt.figure(figsize=(21,15))
 ax = fig.add_subplot(111, projection=proj)
 ax.coastlines()
 cbar_kwargs = {'orientation': 'horizontal', 'label': ''}
-koeppen_simple.plot(ax=ax, cmap=cmap, cbar_kwargs=cbar_kwargs)
-ax.scatter(data_hist.lon, data_hist.lat, transform=proj, c='red', marker='v', s=10)
-ax.scatter(data_active.lon, data_active.lat, transform=proj, c='blue', marker='v', s=10)
+koeppen_simple.plot(ax=ax, cmap=cmap, cbar_kwargs=cbar_kwargs, transform=transf)
+ax.scatter(data.lon, data.lat, transform=transf, c='blue', marker='v', s=10)
 ax.set_title('ISMN station network', fontsize=fs)
 #ax.set_ticklabels(['a','b'])
 plt.savefig('worldmap_stations.png')
