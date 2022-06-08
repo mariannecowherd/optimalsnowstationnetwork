10cf3c2e934f4ce04f1dad6909ee5fe27723ca52
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2022-06-08 09:20:40 +0200

    init commit

---
+++ run_climfill.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#modelnames='IPSL-CM6A-LR HadGEM3-GC31-MM MIROC6 MPI-ESM1-2-HR'
+modelnames='HadGEM3-GC31-MM MIROC6 MPI-ESM1-2-HR IPSL-CM6A-LR ACCESS-ESM1-5 BCC-CSM2-MR CESM2 CMCC-ESM2 CNRM-ESM2-1 CanESM5 E3SM-1-1 FGOALS-g3 GFDL-ESM4 GISS-E2-1-H INM-CM4-8 UKESM1-0-LL'
+metrics='corr seasonality trend'
+strategies='random interp systematic'
+for modelname in $modelnames; do
+    for strategy in $strategies; do
+        for metric in $metrics; do
+            echo "$modelname $strategy $metric"
+            python upscale_cmip_systematic.py --model $modelname --metric $metric --method $strategy
+        done
+    done
+done
