4be1016bb6c1a6f7ca8fc3fb8a00d82d8d530516
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2022-07-19 10:15:28 +0200

    namelist settings

--- run_upscale_cmip.sh
+++ run_upscale_cmip.sh
@@ -3,8 +3,9 @@
 modelnames='HadGEM3-GC31-MM MIROC6 MPI-ESM1-2-HR IPSL-CM6A-LR ACCESS-ESM1-5 BCC-CSM2-MR CESM2 CMCC-ESM2 CNRM-ESM2-1 CanESM5 E3SM-1-1 FGOALS-g3 GFDL-ESM4 GISS-E2-1-H INM-CM4-8 UKESM1-0-LL'
 #modelnames='HadGEM3-GC31-MM MPI-ESM1-2-HR IPSL-CM6A-LR ACCESS-ESM1-5 BCC-CSM2-MR CESM2 CMCC-ESM2 CNRM-ESM2-1 CanESM5 E3SM-1-1 FGOALS-g3 GFDL-ESM4 GISS-E2-1-H INM-CM4-8 UKESM1-0-LL'
 #metrics='corr seasonality trend r2'
-metrics='r2'
-strategies='random interp systematic'
+metrics='seasonality trend r2'
+#strategies='random interp systematic'
+strategies='systematic'
 for modelname in $modelnames; do
     for strategy in $strategies; do
         for metric in $metrics; do
