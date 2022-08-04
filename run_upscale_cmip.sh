55e80e695f5aea4f37b932c00d5fc54f0412708c
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2022-08-04 14:24:32 +0200

    namelist switches

--- run_upscale_cmip.sh
+++ run_upscale_cmip.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-#modelnames='IPSL-CM6A-LR HadGEM3-GC31-MM MIROC6 MPI-ESM1-2-HR'
 modelnames='HadGEM3-GC31-MM MIROC6 MPI-ESM1-2-HR IPSL-CM6A-LR ACCESS-ESM1-5 BCC-CSM2-MR CESM2 CMCC-ESM2 CNRM-ESM2-1 CanESM5 E3SM-1-1 FGOALS-g3 GFDL-ESM4 GISS-E2-1-H INM-CM4-8 UKESM1-0-LL'
-#modelnames='HadGEM3-GC31-MM MPI-ESM1-2-HR IPSL-CM6A-LR ACCESS-ESM1-5 BCC-CSM2-MR CESM2 CMCC-ESM2 CNRM-ESM2-1 CanESM5 E3SM-1-1 FGOALS-g3 GFDL-ESM4 GISS-E2-1-H INM-CM4-8 UKESM1-0-LL'
 #metrics='corr seasonality trend r2'
-metrics='seasonality trend r2'
+metrics='corr'
 #strategies='random interp systematic'
 strategies='systematic'
 for modelname in $modelnames; do
