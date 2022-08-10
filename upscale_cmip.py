cbe678d0ef47a465e0575e2ee689a3f3332deaf2
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2022-08-10 14:13:04 +0200

    save niter and corrmap with additional dimensions

--- upscale_cmip.py
+++ upscale_cmip.py
@@ -43,16 +43,8 @@ logging.info(f'method {method} metric {metric} modelname {modelname}...')
 logging.info('read cmip ...')
 mrso = xr.open_dataset(f'{upscalepath}mrso_{modelname}.nc')['mrso'].load().squeeze()
 pred = xr.open_dataset(f'{upscalepath}pred_{modelname}.nc').load().squeeze()
-
-# detrend
-p = mrso.polyfit('time', deg=1)
-fit = xr.polyval(mrso.time, p.polyfit_coefficients)
-mrso_detr = mrso - fit
-
-p = pred.polyfit('time', deg=1)
-fit = xr.polyval(pred.time, p) #mhauser verified this. bug not feature
-fit = fit.rename_vars({key: key.replace("_polyfit_coefficients", "") for key in fit.data_vars})
-pred_detr = pred - fit
+#landmask = ~np.isnan(mrso.mean(dim='time')) # models need individual landmasks
+landmask = xr.open_dataset(f'{upscalepath}landmask.nc').to_array().squeeze()
 
 # calc standardised anomalies
 # tree-based methods do not need standardisation see https://datascience.stack
@@ -65,27 +57,6 @@ mrso_std = mrso.groupby('time.month').std()
 pred_mean = pred.groupby('time.month').mean()
 pred_std = pred.groupby('time.month').std() 
 
-#if metric == 'corr':
-#    mrso = mrso_detr.groupby('time.month') - mrso_mean
-#    mrso = mrso_detr.groupby('time.month') / mrso_std
-#    pred = pred_detr.groupby('time.month') - pred_mean
-
-#elif metric == 'seasonality':
-#    mrso = mrso_mean / mrso_std
-#    pred = pred_mean# / pred_std
-#
-#    mrso = mrso.rename(month='time')
-#    pred = pred.rename(month='time')
-#
-#elif metric == 'trend':
-#    mrso = mrso.groupby('time.month') - mrso_mean
-#    mrso = mrso.groupby('time.month') / mrso_std
-#    pred = pred.groupby('time.month') - pred_mean
-#
-#    mrso = mrso.resample(time='1y').mean()
-#    pred = pred.resample(time='1y').mean()
-#else:
-#    raise AttributeError('metric not known')
 logging.info(f'metric {metric} data shape {mrso.shape}')
 
 # read station data
@@ -99,34 +70,25 @@ inactive_networks = ['HOBE','PBO_H20','IMA_CAN1','SWEX_POLAND','CAMPANIA',
                      'CHINA','IOWA','RUSWET-VALDAI','RUSWET-GRASS']
 stations = stations.where(~stations.network.isin(inactive_networks), drop=True)
 
-
-# calc min distance to closest station
+# create iter objects
 latlist = stations.lat_cmip.values.tolist() # only needed for first iteration
 lonlist = stations.lon_cmip.values.tolist()
-corrlist = []
-numberlist = []
+niter = xr.full_like(landmask.astype(float), np.nan)
 n = 100
-#for i in range(101):
 i = 0
-lats_added = []
-lons_added = []
+testcase = '_new'
+corrmaps = []
+
 logging.info('start loop ...')
 while True:
+
     # create obsmask and unobsmask
-    #logging.info('divide into obs and unobs ...')
-    #landmask = xr.open_dataset(f'{largefilepath}landmask_cmip6-ng.nc')['mrso'].squeeze()
-    landmask = ~np.isnan(mrso.mean(dim='time')) # models need individual landmasks
     obsmask = xr.full_like(landmask, False)
     unobsmask = landmask.copy(deep=True)
     for lat, lon in zip(latlist, lonlist):
         unobsmask.loc[lat,lon] = False
         if landmask.loc[lat,lon].item(): # obs gridpoint if station contained and on CMIP land 
             obsmask.loc[lat,lon] = True
-
-    if i == 0:
-        obsmask.to_netcdf(f'{largefilepath}opscaling/obsmask_{modelname}.nc')
-        landmask.to_netcdf(f'{largefilepath}opscaling/landmask_{modelname}.nc')
-        quit() # DEBUG
 
     # divide into obs and unobs gridpoints
     obslat, obslon = np.where(obsmask)
@@ -216,12 +178,6 @@ while True:
 
         corr = xr.corr(y_test, y_predict, dim='time')
         corr_train = xr.corr(y_train, y_train_predict, dim='time')
-        #test_seas = y_test.groupby("time.month").mean() 
-        #predict_seas = y_predict.groupby("time.month").mean() 
-        #train_seas = y_train.groupby("time.month").mean() 
-        #train_predict_seas = y_train_predict.groupby("time.month").mean() 
-        #corr = xr.corr(test_seas, predict_seas, dim='month')
-        #corr_train = xr.corr(train_seas, train_predict_seas, dim='month')
     elif metric == 'trend': # TODO until like in Cook 2020: anomalies with reference to baseline period (1851-1880)
         y_test = y_test.resample(time='1y').mean()
         y_predict = y_predict.resample(time='1y').mean()
@@ -239,8 +195,6 @@ while True:
         # R2 should not be used for non-linear models, as it is only pearson**2
         # for linear models and can lead to incorrect conclusions in nonlinear
         # cases see: https://stackoverflow.com/questions/893657/how-do-i-calculate-r-squared-using-python-and-numpy
-        #corr = r2_score(y_test.T,y_predict.T, multioutput='raw_values')
-        #corr_train = r2_score(y_train.T,y_train_predict.T, multioutput='raw_values')
         corr = xr.corr(y_test, y_predict, dim='time')
         corr_train = xr.corr(y_train, y_train_predict, dim='time')
     else:
@@ -248,16 +202,7 @@ while True:
     corrmap[unobslat, unobslon] = corr
     corrmap[obslat, obslon] = corr_train
 
-    # find gridpoint with minimum performance
-    #landpt = np.where(corr.min() == corr)[0].item()
-    #lat = y_test.where(y_test.landpoints == landpt, drop=True).coords["lat"].values[0][0]
-    #lon = y_test.where(y_test.landpoints == landpt, drop=True).coords["lon"].values[0][0]
-    #logging.info(f'{lat}, {lon}')
-    #latlist.append(lat)
-    #lonlist.append(lon)
-
     # find x gridpoints with minimum performance
-    numberlist.append(len(latlist))
     if method == 'systematic':
         landpts = np.argsort(corr)[:n]
     elif method == 'random':
@@ -275,66 +220,41 @@ while True:
         landpts = np.argsort(mindist)[-n:] 
     else:
         raise AttributeError('method not known')
+
     lats = y_latlon.where(y_latlon.landpoints.isin(landpts), drop=True).coords["lat"].values[:,0]
     lons = y_latlon.where(y_latlon.landpoints.isin(landpts), drop=True).coords["lon"].values[:,0]
-    #logging.info(f'{lats}, {lons}')
     latlist = latlist + lats.tolist()
     lonlist = lonlist + lons.tolist()
-    lats_added.append(lats.tolist())
-    lons_added.append(lons.tolist())
 
-    # calc mean corr for log and res
+    # calc mean corr for log 
     if metric == 'trend':
         mean_corr = -corrmap.mean().item()
     else:
         mean_corr = corrmap.mean().item()
-    corrlist.append(mean_corr)
     logging.info(f'iteration {i} obs landpoints {len(latlist)} mean metric {mean_corr}')
 
-    # save intermediate results
-    testcase = '_new'
-    with open(f'corr_{method}_{modelname}_{metric}{testcase}.pkl','wb') as f:
-        pickle.dump(corrlist, f)
-
-    with open(f'nobs_{method}_{modelname}_{metric}{testcase}.pkl','wb') as f:
-        pickle.dump(numberlist, f)
-
-    with open(f'lats_{method}_{modelname}_{metric}{testcase}.pkl','wb') as f:
-        pickle.dump(lats_added, f)
-
-    with open(f'lons_{method}_{modelname}_{metric}{testcase}.pkl','wb') as f:
-        pickle.dump(lons_added, f)
-
-    # plot
-    #proj = ccrs.Robinson()
-    #transf = ccrs.PlateCarree()
-    #fig = plt.figure(figsize=(10,5))
-    #ax = fig.add_subplot(111, projection=proj)
-    #if metric in ['corr','seasonality','r2']:
-    #    corrmap.plot(ax=ax, cmap='coolwarm', transform=transf, vmin=-1, vmax=1)
-    #elif metric == 'trend':
-    #    (-corrmap).plot(ax=ax, cmap='coolwarm', transform=transf, vmin=0, vmax=0.1)
-    #else:
-    #    raise AttributeError('method not known')
-    #ax.coastlines()
-    #ax.set_global()
-    #ax.set_title(f'iter {i} mean corr {np.round(mean_corr,2)}')
-    #im = ax.scatter(lonlist, latlist, c='grey', transform=transf, marker='x', s=5)
-    #im = ax.scatter(lons, lats, c='black', transform=transf, marker='x', s=5)
-    #plt.savefig(f'corr_{i:03}_{method}_{modelname}_{metric}.png')
-    #plt.close()
-    ##plt.show()
-    i += 1
+    # save results
     frac_obs = mrso_obs.shape[1] / (mrso_obs.shape[1] + mrso_unobs.shape[1])
     corrmap = corrmap.assign_coords(frac_observed=frac_obs)
-    corrmap.to_netcdf(f'corrmap_{i:03}_{method}_{modelname}_{metric}{testcase}.nc')
+    # assign scalar coords bug: https://stackoverflow.com/questions/58858083/how-to-create-scalar-dimension-in-xarray
+    corrmap = corrmap.expand_dims({'model': 1, 'metric': 1, 'strategy': 1}).copy()
+    corrmap = corrmap.assign_coords({'model': np.atleast_1d(modelname),
+                                     'metric': np.atleast_1d(metric), 
+                                     'strategy': np.atleast_1d(method)}) 
+    corrmaps.append(corrmap)
+    for lat, lon in zip(lats, lons):
+        niter.loc[lat,lon] = i
 
-# save as netcdf
-#mrso_pred.to_netcdf(f'{largefilepath}mrso_fut_{modelname}_{experimentname}_{ensemblename}.nc') 
-#mrso.to_netcdf(f'{largefilepath}mrso_orig_{modelname}_{experimentname}_{ensemblename}.nc')
+    # set iter
+    i += 1
 
-# plot
-#import IPython; IPython.embed()
-#fig = plt.figure(figsize=(10,5))
-#ax = fig.add_subplot(111)
-#ax.scatter(numberlist,corrlist)
+niter = niter.expand_dims({'model': 1, 'metric': 1, 'strategy': 1}).copy()
+niter = niter.assign_coords({'model': np.atleast_1d(modelname),
+                             'metric': np.atleast_1d(metric), 
+                             'strategy': np.atleast_1d(method)}) 
+
+corrmaps = xr.concat(corrmaps, dim='frac_observed')
+corrmaps = corrmaps.to_dataset(name="mrso")
+corrmaps.to_netcdf(f'corrmap_{method}_{modelname}_{metric}{testcase}.nc')
+niter = niter.to_dataset(name="mrso")
+niter.to_netcdf(f'niter_{method}_{modelname}_{metric}{testcase}.nc')
