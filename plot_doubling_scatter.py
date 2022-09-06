e5d47436acf0d347f6659f31b12bb3af8985367a
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2022-09-06 11:46:56 +0200

    import obsmask instead of calc, remove commented code parts, add AR6 scatter plot again, add testcase in savefig label

--- plot_doubling_scatter.py
+++ plot_doubling_scatter.py
@@ -18,10 +18,11 @@ col_real = colors[0,:]
 
 # load files
 largefilepath = '/net/so4/landclim/bverena/large_files/'
-testcase = 'new'
+testcase = 'smcoup'
 corrmaps = xr.open_mfdataset(f'corrmap_systematic_*_corr_{testcase}.nc', coords='minimal').squeeze().mrso
 niter = xr.open_mfdataset(f'niter_systematic_*_corr_{testcase}.nc', coords='minimal').squeeze().mrso
-landmask = xr.open_dataarray(f'{largefilepath}opscaling/landmask.nc').squeeze()
+#landmask = xr.open_dataarray(f'{largefilepath}opscaling/landmask.nc').squeeze()
+obsmask = xr.open_dataarray(f'{largefilepath}opscaling/obsmask.nc').squeeze()
 
 # extract current and double corr
 min_frac = min(corrmaps.frac_observed)
@@ -36,7 +37,8 @@ niter = niter / niter.max(dim=("lat", "lon")) # removed 1 - ...
 meaniter = niter.mean(dim='model')
 
 # calc obsmask
-obsmask = (np.isnan(meaniter) & landmask)
+#import IPython; IPython.embed()
+#obsmask = (np.isnan(meaniter) & landmask)
 
 # mask for double frac
 meaniter = meaniter < double_frac
@@ -48,36 +50,13 @@ regions = regions.where(~np.isnan(landmask))
 koeppen = xr.open_dataarray(f'{largefilepath}opscaling/koeppen_simple.nc')
 countries = regionmask.defined_regions.natural_earth_v5_0_0.countries_110.mask(orig.lon, orig.lat)
 countries = countries.where(~np.isnan(landmask))
-pop = xr.open_dataarray(f'{largefilepath}opscaling/population_density_regridded.nc')
-crop = xr.open_dataarray(f'{largefilepath}opscaling/cropland_regridded.nc')
-agpop = ((crop > 50) | (pop > 30))
-agpop = agpop.drop(['band','spatial_ref','raster']).squeeze()
 
 # area per grid point
 grid = calc_area(regions)
-#grid_k = calc_area(koeppen.lat.values, koeppen.lon.values)
-
-# area per grid point KOEPPEN
-#res = np.abs(np.diff(koeppen.lat)[0]) # has to be resolution of "regions" for correct grid area calc
-#grid_k = xr.Dataset({'lat': (['lat'], koeppen.lat.data),
-#                   'lon': (['lon'], koeppen.lon.data)})
-#shape = (len(grid_k.lat),len(grid_k.lon))
-#earth_radius = 6371*1000 # in m
-#weights = np.cos(np.deg2rad(grid_k.lat))
-#area = earth_radius**2 * np.deg2rad(res) * weights * np.deg2rad(res)
-#area = np.repeat(area.values, shape[1]).reshape(shape)
-#grid_k['area'] = (['lat','lon'], area)
-#grid_k = grid_k.to_array() / (1000*1000) # to km**2
-#grid_k = grid_k / (1000*1000) # to Mio km**2
 
 # regrid for koeppen
 regridder = xe.Regridder(koeppen, orig, 'bilinear', reuse_weights=False)
 koeppen = regridder(koeppen)
-#regridder = xe.Regridder(orig, koeppen, 'bilinear', reuse_weights=False)
-#orig_highres = regridder(orig)
-#double_highres = regridder(double)
-#obsmask_highres = regridder(obsmask)
-#meaniter_highres = regridder(meaniter)
 
 # station density current and future
 den_ar6_current = obsmask.groupby(regions).sum() / grid.groupby(regions).sum()
@@ -86,8 +65,6 @@ den_ar6_future = (obsmask | meaniter).groupby(regions).sum() / grid.groupby(regi
 den_countries_current = obsmask.groupby(countries).sum() / grid.groupby(countries).sum()
 den_countries_future = (obsmask | meaniter).groupby(countries).sum() / grid.groupby(countries).sum()
 
-#den_koeppen_current = obsmask_highres.groupby(koeppen).sum() / grid_k.groupby(koeppen).sum()
-#den_koeppen_future = (obsmask_highres | meaniter_highres).groupby(koeppen).sum() / grid_k.groupby(koeppen).sum()
 den_koeppen_current = obsmask.groupby(koeppen).sum() / grid.groupby(koeppen).sum()
 den_koeppen_future = (obsmask | meaniter).groupby(koeppen).sum() / grid.groupby(koeppen).sum()
 
@@ -98,17 +75,12 @@ koeppen_names = ['Ocean','Af','Am','Aw','BW','BS','Cs','Cw','Cf','Ds','Dw','Df',
 
 # group by region
 orig_ar6 = orig.groupby(regions).mean()
-#orig_koeppen = orig_highres.groupby(koeppen).mean()
 orig_koeppen = orig.groupby(koeppen).mean()
 orig_countries = orig.groupby(countries).mean()
-orig_agpop = orig.groupby(agpop).mean()
 
 double_ar6 = double.groupby(regions).mean()
-#double_koeppen = double_highres.groupby(koeppen).mean()
 double_koeppen = double.groupby(koeppen).mean()
 double_countries = double.groupby(countries).mean()
-double_agpop = double.groupby(agpop).mean()
-import IPython; IPython.embed()
 
 # drop desert and ice regions from koeppen
 orig_koeppen = orig_koeppen.drop_sel(group=[0,4,12,13])
@@ -149,64 +121,65 @@ legend_elements = [Line2D([0], [0], marker='o', color='w',
                    markeredgecolor=col_random, alpha=1, markersize=10)]
 
 # plot
-fig = plt.figure(figsize=(10,5))
-ax = fig.add_subplot(111)
-
-ax.grid(0.2)
-
-ax.set_title('(c) Koppen-Geiger climates')
-
-for x1,y1,x2,y2 in zip(den_koeppen_current,orig_koeppen,den_koeppen_future,double_koeppen):
-    ax.plot([x1, x2], [y1, y2], c=col_random, alpha=a)
-for label,x,y in zip(koeppen_names, den_koeppen_future, double_koeppen):
-    ax.text(x=x,y=y,s=label)
-ax.scatter(den_koeppen_current,orig_koeppen, c=col_random, edgecolor='black', alpha=a)
-ax.scatter(den_koeppen_future,double_koeppen, c=col_random)
-
-ax.set_xlabel('stations per Mio $km^2$')
-
-ax.set_ylabel('pearson correlation')
-ax.set_ylim([0.2,0.7])
-
-ax. legend(handles=legend_elements)
-
-plt.savefig('doubling_scatter.pdf', bbox_inches='tight')
-
-# plot
-#fig = plt.figure(figsize=(10,10))
-#ax1 = fig.add_subplot(211)
-#ax2 = fig.add_subplot(212)
+#fig = plt.figure(figsize=(10,5))
+#ax = fig.add_subplot(111)
 #
-#ax1.grid(0.2)
-#ax2.grid(0.2)
+#ax.grid(0.2)
 #
-#ax1.set_title('(c) Koppen-Geiger climates')
-#ax2.set_title('(d) AR6 regions')
+#ax.set_title('(c) Koppen-Geiger climates')
 #
 #for x1,y1,x2,y2 in zip(den_koeppen_current,orig_koeppen,den_koeppen_future,double_koeppen):
-#    ax1.plot([x1, x2], [y1, y2], c=col_random, alpha=a)
+#    ax.plot([x1, x2], [y1, y2], c=col_random, alpha=a)
 #for label,x,y in zip(koeppen_names, den_koeppen_future, double_koeppen):
-#    ax1.text(x=x,y=y,s=label)
-#ax1.scatter(den_koeppen_current,orig_koeppen, c=col_random, edgecolor='black', alpha=a)
-#ax1.scatter(den_koeppen_future,double_koeppen, c=col_random)
+#    ax.text(x=x,y=y,s=label)
+#ax.scatter(den_koeppen_current,orig_koeppen, c=col_random, edgecolor='black', alpha=a)
+#ax.scatter(den_koeppen_future,double_koeppen, c=col_random)
 #
+#ax.set_xlabel('stations per Mio $km^2$')
 #
-#for x1,y1,x2,y2 in zip(den_ar6_current,orig_ar6,den_ar6_future,double_ar6):
-#    ax2.plot([x1, x2], [y1, y2], c=col_random, alpha=a)
-#for label,x,y in zip(region_names, den_ar6_future, double_ar6):
-#    ax2.text(x=x,y=y,s=label)
-#ax2.scatter(den_ar6_current,orig_ar6, c=col_random, edgecolor='black', alpha=a)
-#ax2.scatter(den_ar6_future,double_ar6, c=col_random)
+#ax.set_ylabel('pearson correlation')
+#ax.set_ylim([0.2,0.7])
+#ax.set_xlim([0,8])
 #
+#ax. legend(handles=legend_elements)
 #
-##ax1.set_xlabel('stations per Mio $km^2$')
-#ax2.set_xlabel('stations per Mio $km^2$')
-#
-#ax1.set_ylabel('pearson correlation')
-#ax2.set_ylabel('pearson correlation')
-#ax1.set_ylim([0.15,0.9])
-#ax2.set_ylim([0.15,0.9])
-#
-#ax1. legend(handles=legend_elements)
-#
-#plt.savefig('doubling_scatter.pdf', bbox_inches='tight')
+#plt.savefig(f'doubling_scatter_{testcase}.pdf', bbox_inches='tight')
+
+# plot
+fig = plt.figure(figsize=(10,10))
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+
+ax1.grid(0.2)
+ax2.grid(0.2)
+
+ax1.set_title('(c) Koppen-Geiger climates')
+ax2.set_title('(d) AR6 regions')
+
+for x1,y1,x2,y2 in zip(den_koeppen_current,orig_koeppen,den_koeppen_future,double_koeppen):
+    ax1.plot([x1, x2], [y1, y2], c=col_random, alpha=a)
+for label,x,y in zip(koeppen_names, den_koeppen_future, double_koeppen):
+    ax1.text(x=x,y=y,s=label)
+ax1.scatter(den_koeppen_current,orig_koeppen, c=col_random, edgecolor='black', alpha=a)
+ax1.scatter(den_koeppen_future,double_koeppen, c=col_random)
+
+
+for x1,y1,x2,y2 in zip(den_ar6_current,orig_ar6,den_ar6_future,double_ar6):
+    ax2.plot([x1, x2], [y1, y2], c=col_random, alpha=a)
+for label,x,y in zip(region_names, den_ar6_future, double_ar6):
+    ax2.text(x=x,y=y,s=label)
+ax2.scatter(den_ar6_current,orig_ar6, c=col_random, edgecolor='black', alpha=a)
+ax2.scatter(den_ar6_future,double_ar6, c=col_random)
+
+
+#ax1.set_xlabel('stations per Mio $km^2$')
+ax2.set_xlabel('stations per Mio $km^2$')
+
+ax1.set_ylabel('pearson correlation')
+ax2.set_ylabel('pearson correlation')
+ax1.set_ylim([0.15,0.9])
+ax2.set_ylim([0.15,0.9])
+
+ax1. legend(handles=legend_elements)
+
+plt.savefig(f'doubling_scatter_{testcase}.pdf', bbox_inches='tight')
