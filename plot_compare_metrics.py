0287f1e88c5a9e818ec4d38b4760dba3a259c88c
Verena Bessenbacher <verena.bessenbacher@env.ethz.ch>
2022-09-08 15:48:08 +0200

    not percentile bars anymore, but maps, only two metrics left

--- plot_compare_metrics.py
+++ plot_compare_metrics.py
@@ -7,8 +7,6 @@ import matplotlib.pyplot as plt
 import regionmask
 
 upscalepath = '/net/so4/landclim/bverena/large_files/opscaling/'
-metrics = ['r2','seasonality','corr','trend']
-metrics = ['corr']
 
 # read files
 largefilepath = '/net/so4/landclim/bverena/large_files/'
@@ -16,6 +14,7 @@ filename = f'{largefilepath}opscaling/koeppen_simple.nc'
 koeppen = xr.open_dataarray(filename)
 testcase = 'smmask'
 niter = xr.open_mfdataset(f'niter_systematic*{testcase}.nc', coords='minimal').squeeze().mrso
+landmask = regionmask.defined_regions.natural_earth_v5_0_0.land_110.mask(niter.lon, niter.lat)
 
 # calc rank percentages from iter
 niter = niter / niter.max(dim=("lat", "lon")) # removed 1 - ...
@@ -24,65 +23,34 @@ niter = niter / niter.max(dim=("lat", "lon")) # removed 1 - ...
 meaniter = niter.mean(dim='model')
 stditer = niter.std(dim='model')
 
-# delete points that are desert in any model # not necessary anymore bec harmonised desert mask
-#meaniter = meaniter.where(~np.isnan(niter).any(dim='model'))
-#niter = niter.where(~np.isnan(niter).any(dim='model'))
-
-# regrid to koeppen grid for bar plot
-#regridder = xe.Regridder(niter, koeppen, 'bilinear', reuse_weights=False)
-#niter = regridder(niter)
-#koeppen_classes = ['Af','Am','Aw','BW','BS','Cs','Cw','Cf','Ds','Dw','Df']
-#koeppen_classes = ['Af','Am','Aw','BS','Cs','Cw','Cf','Ds','Dw','Df'] # BW is removed
-#koeppen_ints = np.arange(1,12).astype(int)
-#koeppen_ints = [1,2,3,5,6,7,8,9,10,11]
-#
-## percentiles across models:
-## groupby_bins with bin int: TypeError: cannot perform reduce with flexible type
-##                       arr: ValueError: None of the data falls within bins with edges [0.5, 0.6]
-##perc = np.floor(niter*10)*10 # np.floor only rounds to integers
-#koeppen_res = []
-#koeppen_res = np.zeros((4,10,10))
-#for m, metric in enumerate(metrics):
-#    for k, i in enumerate(koeppen_ints):
-#        tmp = niter.sel(metric=metric).where(koeppen == i).stack(land=('lat','lon')).dropna('land', how='all')
-#        tmp = tmp.values.flatten()
-#        tmp = np.histogram(tmp, bins=np.arange(0,1.1,0.1))[0]
-#        koeppen_res[m,k,:] = tmp / tmp.sum()
-#koeppen_res[0,:,:] = koeppen_res[0,:,:] / koeppen_res[0,:,:].sum(axis=0)
-#koeppen_res[1,:,:] = koeppen_res[1,:,:] / koeppen_res[1,:,:].sum(axis=0)
-#koeppen_res[2,:,:] = koeppen_res[2,:,:] / koeppen_res[2,:,:].sum(axis=0)
-#koeppen_res[3,:,:] = koeppen_res[3,:,:] / koeppen_res[3,:,:].sum(axis=0)
+# set ocean negative such that blue on map
+meaniter = meaniter.where(~np.isnan(landmask), -10)
+stditer = stditer.where(~np.isnan(landmask), -10)
 
 # plot
+fs = 25
 proj = ccrs.Robinson()
 transf = ccrs.PlateCarree()
-cmap = plt.get_cmap('Reds_r')
+cmap_r = plt.get_cmap('Reds_r').copy()
+cmap = plt.get_cmap('Reds').copy()
+bad_color = 'lightgrey'
+cmap_r.set_under('aliceblue')
+cmap.set_under('aliceblue')
 
-colors = np.array([[81,73,171],[124,156,172],[236,197,140],[85,31,50],[189,65,70],[243,220,124]])
-colors = colors/255.
-col_random = colors[4,:]
-col_swaths = colors[2,:]
-col_real = colors[0,:]
-
-fig = plt.figure(figsize=(10, 10))
-ax1 = fig.add_subplot(421, projection=proj)
-ax2 = fig.add_subplot(423, projection=proj)
-ax3 = fig.add_subplot(425, projection=proj)
-ax4 = fig.add_subplot(427, projection=proj)
-ax5 = fig.add_subplot(422, projection=proj)
-ax6 = fig.add_subplot(424, projection=proj)
-ax7 = fig.add_subplot(426, projection=proj)
-ax8 = fig.add_subplot(428, projection=proj)
+fig = plt.figure(figsize=(10, 5))
+ax1 = fig.add_subplot(221, projection=proj)
+ax2 = fig.add_subplot(222, projection=proj)
+ax3 = fig.add_subplot(223, projection=proj)
+ax4 = fig.add_subplot(224, projection=proj)
 
 levels = np.arange(0.0,1.1,0.1)
-meaniter[0,:,:].plot(ax=ax1, add_colorbar=False, cmap=cmap, vmin=0, vmax=1, transform=transf, levels=levels)
-im = meaniter[1,:,:].plot(ax=ax2, add_colorbar=False, cmap=cmap, vmin=0, vmax=1, transform=transf, levels=levels)
-#meaniter[2,:,:].plot(ax=ax3, add_colorbar=False, cmap=cmap, vmin=0, vmax=1, transform=transf, levels=levels)
-#im = meaniter[3,:,:].plot(ax=ax4, add_colorbar=False, cmap=cmap, vmin=0, vmax=1, transform=transf, levels=levels)
-stditer[0,:,:].plot(ax=ax5, add_colorbar=False, cmap=cmap, vmin=0, vmax=1, transform=transf, levels=levels)
-stditer[0,:,:].plot(ax=ax6, add_colorbar=False, cmap=cmap, vmin=0, vmax=1, transform=transf, levels=levels)
-#stditer[0,:,:].plot(ax=ax7, add_colorbar=False, cmap=cmap, vmin=0, vmax=1, transform=transf, levels=levels)
-#stditer[0,:,:].plot(ax=ax8, add_colorbar=False, cmap=cmap, vmin=0, vmax=1, transform=transf, levels=levels)
+stdmin=0.2
+stdmax=0.45
+levels_std = np.arange(stdmin,stdmax,0.05)
+meaniter[0,:,:].plot(ax=ax1, add_colorbar=False, cmap=cmap_r, vmin=0, vmax=1, transform=transf, levels=levels)
+stditer[0,:,:].plot(ax=ax2, add_colorbar=False, cmap=cmap, vmin=stdmin, vmax=stdmax, transform=transf, levels=levels_std)
+im1 = meaniter[1,:,:].plot(ax=ax3, add_colorbar=False, cmap=cmap_r, vmin=0, vmax=1, transform=transf, levels=levels)
+im2 = stditer[1,:,:].plot(ax=ax4, add_colorbar=False, cmap=cmap, vmin=stdmin, vmax=stdmax, transform=transf, levels=levels_std)
 
 ax1.coastlines()
 ax2.coastlines()
@@ -90,47 +58,26 @@ ax3.coastlines()
 ax4.coastlines()
 
 ax1.set_title('(a)')
-ax2.set_title('(c)')
-ax3.set_title('(e)')
-ax4.set_title('(g)')
-ax5.set_title('(b)')
-ax6.set_title('(d)')
-ax7.set_title('(f)')
-ax8.set_title('(h)')
-ax1.text(-0.4, 0.5,'Monthly mean',transform=ax1.transAxes, va='center')
-ax2.text(-0.4, 0.5,'Mean \nseasonal cycle',transform=ax2.transAxes, va='center')
-ax3.text(-0.4, 0.5,'Monthly \nanomalies',transform=ax3.transAxes, va='center')
-ax4.text(-0.4, 0.5,'Long-term trend',transform=ax4.transAxes, va='center')
+ax2.set_title('(b)')
+ax3.set_title('(c)')
+ax4.set_title('(d)')
+ax1.text(-0.3, 0.5,'Interannual \nvariability',transform=ax1.transAxes, va='center')
+ax3.text(-0.3, 0.5,'Long-term \ntrend',transform=ax3.transAxes, va='center')
 
-cbar_ax = fig.add_axes([0.15, 0.07, 0.3, 0.02]) # left bottom width height
-cbar = fig.colorbar(im, cax=cbar_ax, orientation='horizontal')
-cbar.set_label('mean rank percentile')
+cbar_ax = fig.add_axes([0.13, 0.1, 0.3, 0.02]) # left bottom width height
+cbar = fig.colorbar(im1, cax=cbar_ax, orientation='horizontal')
+cbar.set_label('Multi-model mean rank percentile')
 
-#for i in range(10):
-#    ax5.bar(np.arange(len(koeppen_ints)), koeppen_res[0,i,:], bottom=koeppen_res[0,:i,:].sum(axis=0), color=cmap(i/10))
-#    ax6.bar(np.arange(len(koeppen_ints)), koeppen_res[1,i,:], bottom=koeppen_res[1,:i,:].sum(axis=0), color=cmap(i/10))  
-#    ax7.bar(np.arange(len(koeppen_ints)), koeppen_res[2,i,:], bottom=koeppen_res[2,:i,:].sum(axis=0), color=cmap(i/10))
-#    ax8.bar(np.arange(len(koeppen_ints)), koeppen_res[3,i,:], bottom=koeppen_res[3,:i,:].sum(axis=0), color=cmap(i/10))
+cbar_ax2 = fig.add_axes([0.59, 0.1, 0.3, 0.02]) # left bottom width height
+cbar = fig.colorbar(im2, cax=cbar_ax2, orientation='horizontal')
+cbar.set_label('Multi-model standard deviation rank percentile')
 
-#ax5.set_ylim([0,0.98])
-#ax6.set_ylim([0,0.98])
-#ax7.set_ylim([0,0.98])
-#ax8.set_ylim([0,0.98])
+ax1.set_facecolor(bad_color)
+ax2.set_facecolor(bad_color)
+ax3.set_facecolor(bad_color)
+ax4.set_facecolor(bad_color)
 
-#ax5.set_xticks(np.arange(len(koeppen_ints)))
-#ax5.set_xticklabels(koeppen_classes)
-#ax6.set_xticks(np.arange(len(koeppen_ints)))
-#ax6.set_xticklabels(koeppen_classes)
-#ax7.set_xticks(np.arange(len(koeppen_ints)))
-#ax7.set_xticklabels(koeppen_classes)
-#ax8.set_xticks(np.arange(len(koeppen_ints)))
-#ax8.set_xticklabels(koeppen_classes)
-#ax5.set_ylabel('mean rank percentile')
-#ax6.set_ylabel('mean rank percentile')
-#ax7.set_ylabel('mean rank percentile')
-#ax8.set_ylabel('mean rank percentile')
+fig.subplots_adjust(hspace=0.1, bottom=0.15, wspace=0.4)
 
-#fig.subplots_adjust(hspace=0.3)
-
-plt.show()
-#plt.savefig('metrics_maps.png')
+#plt.show()
+plt.savefig('metrics.png', dpi=300)
